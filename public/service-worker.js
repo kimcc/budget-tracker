5475534e4483630cec9942f209b5b263013775e2
kimcc <15278020+kimcc@users.noreply.github.com>
2021-11-07 18:07:31 -0600

    add service worker and index db files

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js"
+];
+
+// Use self to refer to the service worker object. Don't use window because the service worker runs before the window object has been created
+self.addEventListener('install', function (e) {
+  // Use e.waitUntil to tell the browser to wait until the work is complete before terminating the service worker
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  );
+});
+
+// clear out any old data from the cache and, in the same step, tell the service worker how to manage caches
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// listen for the fetch event, log the URL of the requested resource, and then define how we will respond to the request.
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
